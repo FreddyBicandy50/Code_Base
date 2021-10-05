572ee7961be0843145d6ff7a887b66ccf68e0a56
FreddyBicandy50 <fredos112033@gmail.com>
2021-10-05 13:05:57 +0300

    new

--- file.c
+++ file.c
@@ -1,19 +1,17 @@
-//calling libraries and predefines
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 typedef struct pointer{
     int data;
     struct pointer *next;
-    struct pointer *prev; 
+    struct pointer *prev;
 }node;
 node *list=NULL;
 const int size=5;
 int fprint_list();
-int init_next(int number){ 
+int init_next(int number){
     node *temp=malloc(sizeof(node));
     if(temp==NULL){
-        printf("temp is NULL!\nsegmentation fault");
+        printf("temp is Null!\nsegmentation fault");
         return 1;
     }
     temp->data=number;
@@ -24,31 +22,16 @@ int init_next(int number){
     list=temp;
     return 0;
 }
-bool init_prev(){
-    node *recent=list;
-    list=list->next;
-    if(list==NULL){
-        return true;
-    }
-    list->prev=recent;
-    return false;
-}
-int main (void){
-    //initalise list next
+int main (void) {
     int check;
     for (int i = 1; i <=size; i++){ 
         check=init_next(i);
         if(check==1){
-            printf("\nexiting...");
+            printf("exiting...");
             exit(-1);
-        } 
+        }
     }
-    bool exit=false;
-    node *last=list;
-    while (exit!=true){
-        exit=init_prev();
-    }
-    list=last;
+    //printing
     fprint_list();
     free(list);
     return 0;
@@ -56,15 +39,7 @@ int main (void){
 int fprint_list(){
     while(list!=NULL){
         printf("%d\t",list->data);
-        if(list->next==NULL){
-            break;
-        }
         list=list->next;
-    }
-    printf("\n");
-    while(list!=NULL){
-        printf("%d\t",list->data);
-        list=list->prev;
     }
     return 0;
 }
