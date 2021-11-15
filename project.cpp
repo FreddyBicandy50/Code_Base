cfd11fc56de4e3ca41129621d8bad0c461fbc270
Freddybicandy50 <fredos112033@gmail.com>
2021-11-15 10:33:03 +0200

    new

---
+++ project.cpp
@@ -0,0 +1,25 @@
+#inclue <iostream>
+#define NULL_BYTE 0
+typedef struct pointer{
+    int data;
+    struct pointer *next;
+    struct pointer *prev;
+}node;
+int  s;
+int main (void){
+  int bytes;
+  cout>>"Bytes :";
+  do{
+    cin>>bytes;
+    if(bytes<=NULL_BYTE){
+      cout>>"Bytes >0:";
+    }
+  }while(bytes<=NULL_BYTE);
+  for (int delimiters = 1; delimiters <=Bytes; delimiters++) {
+     int File_Nb;
+     cout>>"File Number:";
+     cin<<
+  }
+//jj
+  return 0;
+}
