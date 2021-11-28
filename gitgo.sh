0e064bd056404c9d01728c1a567da3ce14a11e83
Freddybicandy50 <fredos112033@gmail.com>
2021-11-28 13:53:51 +0200

    new

--- gitgo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#git config --global credential.helper.cache
-cp -f project.cpp gitgo.sh /home/null/Code
-cd nordlight-syntax/styles
-cp -f base.less colors.less /home/null/Code
-cd /home/null/Code
-sudo bash push.sh
