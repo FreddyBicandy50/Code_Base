d04057232fd4ee680380d2020c0cffddf0960e1e
Freddybicandy50 <fredos112033@gmail.com>
2021-11-28 18:24:13 +0200

    Directory

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 #git config --global credential.helper.cache
 git status
 git add push.sh
-git add -r Projects
+git add Projects
 git commit -m "Directory"
 git branch -M main 
 git push -f origin main
