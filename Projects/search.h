905588d03906a10804b34e9aadf178d1804a8096
Freddybicandy50 <fredos112033@gmail.com>
2021-11-29 17:37:50 +0200

    wsl

--- Projects/search.h
+++ Projects/search.h
@@ -1,10 +1,106 @@
 #ifndef search_H
-#define search_H  
- 
+#define search_H
+//calling libraries and predefines
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
+#include <cstdio>
+#define INDEXED_Position 0
+#define BUFFER_Position_Size 1
+#define Position_VALUE 2
+#define sizeofstrlen 9
+char FILENAME[sizeofstrlen] = "list.txt";
+FILE *File_Var = fopen(FILENAME, "r");
+using namespace std;
+
+//usermame and password lists
+typedef struct pointer
+{
+    string data;
+    struct pointer *next;
+} node;
+node *list_usernames = NULL;
+node *list_passwords = NULL;
+int search_unit(string username,string password);
+
+
+//initiliase name in singly-linked-list
+bool init_name_list(string Name_HOLDER)
+{
+    node *temp = (node *)malloc(sizeof(node));
+    if (temp == NULL)
+    {
+        cout << "Malloc Failure" << endl;
+    }
+    temp->data = Name_HOLDER;
+    temp->next = list_usernames;
+    while (list_usernames != NULL)
+    {
+        list_usernames = list_usernames->next;
+    }
+    list_usernames = temp;
+    return true;
+}
+
+//initiliase passwords in singly-linked-list
+bool init_password_list(string Password_HOLDER){
+    node *temp = (node *)malloc(sizeof(node));
+    if (temp == NULL)
+    {
+        cout << "Error <Malloc Failure!>" << endl;
+    }
+    temp->data = Password_HOLDER;
+    temp->next = list_passwords;
+    while (list_passwords != NULL)
+    {
+        list_passwords = list_passwords->next;
+    }
+    list_passwords = temp;
+    return true;
+}
+
+//main function
+int init_SearchChain(void){
+    //extracting list from database
+    if (File_Var == NULL)
+    {
+        cout << "FATAL:Error Search Stream!/>>IOFile Not Found!>>" << endl;
+        cout << "Download File:https://github.com/FreddyBicandy50/MyLibrary/blob/main/Projects/list.txt" << endl;
+    }
+    char *Char_HOLDER = (char *)malloc(sizeof(char) * sizeofstrlen * 100);
+    static int PlaceHOLDER = INDEXED_Position;
+    while (fscanf(File_Var, "%s", Char_HOLDER) == 1){
+        if (PlaceHOLDER % Position_VALUE == INDEXED_Position){
+            init_name_list(Char_HOLDER);
+        }
+        else{
+            init_password_list(Char_HOLDER);
+        }
+        PlaceHOLDER += BUFFER_Position_Size;
+    }
+
+    //1.search
+    //2.Main Activity
+    //3.register
+    return 0;
+}
+
 int Cryptlib(int CallFunction,string username,string password){
     if (CallFunction==1){
-        search(username,password);
+        init_SearchChain();
+        search(username, password);
     }
     return 0;
+}
+
+int search(string username,string password){
+    //username
+    while (list_usernames!=NULL){
+        if(sctrmp(username,list_usernames->data)==0){
+            
+        }
+    }
+        return 0;
 }
 #endif
--- Projects/test.cpp
+++ Projects/test.cpp
@@ -5,9 +5,12 @@
 #include <cctype>
 #include <cstdio>
 #define INDEXED_Position 0
+#define RUNTIME_FAILURE false
 #define BUFFER_Position_Size 1
 #define Position_VALUE 2
 #define sizeofstrlen 9
+#define UNLOCK_user 10
+
 char FILENAME[sizeofstrlen] = "list.txt";
 FILE *File_Var = fopen(FILENAME, "r");
 using namespace std;
@@ -16,59 +19,100 @@ using namespace std;
 typedef struct pointer{
     string data;
     struct pointer *next;
-}node;
-node *list_usernames=NULL;
-node *list_passwords=NULL;
+} node;
+node *list_usernames = NULL;
+node *list_passwords = NULL;
+int search_unit(string username, string password);
 
 //initiliase name in singly-linked-list
-bool init_name_list(string Name_HOLDER){
-    node *temp=(node *) malloc(sizeof(node));
-    if(temp==NULL){
-        cout<< "Malloc Failure" <<endl;
+bool init_list(string VAr_HOLDER, static int keylogger){
+    node *temp = (node *)malloc(sizeof(node));
+    if (temp == NULL){
+        cout << "Error <Malloc Failure!>" << endl;
+        return false;
     }
-    temp->data=Name_HOLDER;
-    temp->next=list_usernames;
-    while(list_usernames!=NULL){
-        list_usernames=list_usernames->next;
+    
+    if(keylogger==BUFFER_Position_Size){
+        temp->data = VAr_HOLDER;
+        temp->next = list_usernames;
+        while (list_usernames != NULL){
+            list_usernames = list_usernames->next;
+        }
+        list_usernames = temp;
+        return true;
+    }else if(keylogger==INDEXED_Position){
+        temp->data = VAr_HOLDER;
+        temp->next = list_passwords;
+        while (list_passwords != NULL)
+        {
+            list_passwords = list_passwords->next;
+        }
+        list_passwords = temp;
+        return true;
+    }else{
+        cout<<"Invalid KEY!"<<endl;
+        return false;
     }
-    list_usernames=temp;
-    return true;
-}
-
-//initiliase passwords in singly-linked-list
-bool init_password_list(string Password_HOLDER){ 
-    node *temp=(node *) malloc(sizeof(node));
-    if(temp==NULL){
-        cout<< "Malloc Failure" <<endl;
-    }
-    temp->data=Password_HOLDER;
-    temp->next=list_passwords;
-    while(list_passwords!=NULL){
-        list_passwords=list_passwords->next;
-    }
-    list_passwords=temp;
-    return true;
 }
 
 //main function
-int main(void){
+int init_SearchChain(void){
     //extracting list from database
     if (File_Var == NULL){
         cout << "FATAL:Error Search Stream!/>>IOFile Not Found!>>" << endl;
         cout << "Download File:https://github.com/FreddyBicandy50/MyLibrary/blob/main/Projects/list.txt" << endl;
     }
-    char *Char_HOLDER= (char *)malloc(sizeof(char)*sizeofstrlen*100);
-    static int PlaceHOLDER=INDEXED_Position;
+    char *Char_HOLDER = (char *)malloc(sizeof(char) * sizeofstrlen * 100);
+    static int keyholder,PlaceHOLDER = INDEXED_Position;
     while (fscanf(File_Var, "%s", Char_HOLDER) == 1){
         if (PlaceHOLDER % Position_VALUE == INDEXED_Position){
-            init_name_list(Char_HOLDER);
+            bool Status_Check = init_list(Char_HOLDER, BUFFER_Position_Size);
+            if(Status_Check==RUNTIME_FAILURE){
+                cout<<"Fatal System error:\n"<<endl;
+                exit(-1);
+            }
         }else{
-            init_password_list(Char_HOLDER);
+            bool Status_Check = init_list(Char_HOLDER, INDEXED_Position);
+            if(Status_Check==RUNTIME_FAILURE){
+                cout<<"Fatal System error:\n"<<endl;
+                exit(-1);
+            }
         }
-        PlaceHOLDER+=BUFFER_Position_Size ;
+        PlaceHOLDER += BUFFER_Position_Size;
     }
+ 
+   
+    return 0;
+}
+
+int main(void){
     //1.search
     //2.Main Activity
     //3.register
+    init_SearchChain();
+    string name="freddy";
+    string password="1120";
+    search(name,password);
     return 0;
 }
+
+int search(string username, string password){
+    //username
+    while (list_usernames!=NULL){
+        if (sctrmp(username, list_usernames->data) == 0){
+            while(list_passwords!=NULL){
+                if(strcmp(password,list_passwords->data)==0){
+                    static int Acces_Granted;
+                }
+                list_passwords=list_passwords->next;
+                return 0;
+            }
+        }
+        list_usernames=list_usernames->next;
+        
+    }
+    if(true){
+        cout<<"Wallet Credential Not match any!\n";
+    }
+    return 0;
+} 
