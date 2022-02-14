c0548b65554ff7890395aed9f2b3efcb5a6f95d4
Freddy Bicandy <79138306+FreddyBicandy50@users.noreply.github.com>
2022-02-14 16:30:04 +0200

    Delete push.sh

--- push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#git config --global credential.helper.cache
-git status
-git add push.sh
-git add ./Projects
-git add ./Computer_profficency
-git commit -m "Directory"
-git branch -M main
-git push -f origin main
