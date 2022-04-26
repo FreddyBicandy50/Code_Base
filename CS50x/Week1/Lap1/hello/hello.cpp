bc9c512e7b211ff9e944f9f42032db808bb6aa93
Freddybicandy50 <fredos112033@gmail.com>
2022-04-26 20:10:52 +0300

    Directory

--- CS50x/Week1/Lap1/hello/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main (void){
-  string name;
-  cout<<"what is your name?";
-  cin>>name;
-  cout<<"hello "<<name<<":)"<<endl; 
-  return 0;
-}
--- CS50x/Week5/Problemset5/hash.h
+++
@@ -1,3 +0,0 @@
-#ifndef hash_H
-
-#endif // !
