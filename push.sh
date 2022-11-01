92c8b623ec72af0f3338f233156ff816fbeec76d
FreddyBicandy50 <fredos112033@gmail.com>
2022-11-01 16:22:50 +0200

    Directory

---
+++ push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#git config --global credential.helper.cache
+git status
+git add --all /home/null/git
+git add push.sh
+git commit -m "Directory"
+git branch -M main
+git push -f origin main
