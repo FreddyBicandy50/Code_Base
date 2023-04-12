9f836b0d28c2e4bee0f8a7fad0a428e0fb92c5bf
FreddyBicandy50 <fredos112033@gmail.com>
2023-04-12 10:30:24 +0300

    save

--- linkdlist.java
+++ linkdlist.java
@@ -1,26 +1,65 @@
+ 
+public class linkdlist{
+      static class Node {
+        int data;
+        Node prev;
+        Node next;
 
-public class linkedlist{
-    
-   public static void main(String[]args){
-        class Node {
-           int data;
-           Node next;   
-       }
-       Node head =null;
-        
-       Node temp1 = new Node();
-       temp1.data = 1;
-       temp1.next=head;
-       head=temp1;
-       Node temp2=new Node();
-       temp2.data=2;
-       head.next=temp2;
-       
-        while(true){
-            if(head.next!=null){
-               System.out.println(head.data);
-            }else break;
-            head=head.next;
-        } 
-}
+        Node(int d) {
+            data = d;
+            prev = null;
+            next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node head = null;
+
+        head = appendFirst(head, 1);
+        head = appendFirst(head, 2);
+        head = appendLast(head, 3);
+        head = appendLast(head, 4);
+
+         System.out.println("NEXT:");
+        while (true) {
+            System.out.println(head.data);
+            if (head.next!=null) head = head.next;
+            else break;
+        }
+
+        System.out.println("PERV:");
+        while (true) {
+            System.out.println(head.data);
+            if(head.prev!=null)head = head.prev;
+            else break;
+        }
+    }
+
+    public static Node appendLast(Node head, int number) {
+        Node newNODE = new Node(number);
+        if (head == null) {
+            head = newNODE;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNODE;
+            newNODE.prev = current;
+        }
+
+        return head;
+    }
+
+    public static Node appendFirst(Node head, int number) {
+        Node newNODE = new Node(number);
+        if (head == null) {
+            head = newNODE;
+        } else {
+            newNODE.next = head;
+            head.prev = newNODE;
+            head = newNODE;
+        }
+        return head;
+    }
 }
