7be902279d44934b5b1b7b1a431a2ffe16bd3b4e
FreddyBicandy50 <fredos112033@gmail.com>
2023-04-28 18:23:39 +0300

    save

--- Python/g12.py
+++
@@ -1,2 +0,0 @@
-#Write a program to display the names of even index
-array=["Natalia","Imad","Freddy","Matteo","Heinz","Jana","Sami","Rola"]
--- theo.py
+++
@@ -1,7 +0,0 @@
-dict={
-       "list1":[1,2,3,4],
-       "list2":[5,6,7,8]
-}
-
-print(dict["list1"][1])
-print(dict["list2"][0])
