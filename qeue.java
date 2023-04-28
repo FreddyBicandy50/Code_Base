d0efa852ef62b68cfd05cf22d0a7ca1a505031ca
FreddyBicandy50 <fredos112033@gmail.com>
2023-04-28 17:52:08 +0300

    pull

--- qeue.java
+++ qeue.java
@@ -1,35 +1,56 @@
 public class qeue {
-    int rear=-1,front=-1;
+
+    int rear=-1,front=-1,temprear;
     int array[];
     public qeue(){
         this.array= new int[5];  
     }
-    public void enqeue(int number) {
-        if rear(+)
+
+    public void enqeue(int number) { 
+        //check if rear is still in range 
+            //if yes check also the next move if empty
+        if(rear+1<=array.length-1 && array[rear+1]!=0){
+            rear=temprear; //go back to last index
+            System.out.println("Array is full");
+            return;
+        }  
+        //check if we are at the begening
+        if(front == -1)front++;
         if (rear == -1) {
             array[0] = number;
             rear++;
         }
-        if(front == -1)front++;
+        
         else {
             if (rear+1 <= array.length - 1) {
-                array[rear + 1] = number;
                 rear++;
+                array[rear] = number;
             }else{
+                temprear=rear; 
                 rear=-1;
                 enqeue(number);
             }
-        }
-
+        } 
+        temprear=rear;
     }
     public void print_array(){
-        System.out.println("\nfront="+array[front]);
-        System.out.println("rear="+array[rear]);
+       
+        if(rear==-1 && front==-1){
+            System.out.println("array is empty!");
+        }else{
+             System.out.println("\nfront="+"["+front+"]="+array[front]);
+                System.out.println("rear"+"["+rear+"]="+array[rear]);
+        }
     }
     public void deqeue(){ 
-        if(front!=-1){
-            int temp=array[front];
-            if(front==array.length-1) front=-1;
+        if(front!=-1){ 
+            array[front]=0;
+            if(rear==front){
+                front=-1;
+                rear=-1;
+                return;
+            }
+            if(front==array.length-1) front=0;
             else front++;
         }
     }   
