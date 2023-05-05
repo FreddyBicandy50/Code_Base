c919a64c77e03863a832f4390a7ba49a4c55eca2
FreddyBicandy50 <fredos112033@gmail.com>
2023-05-05 08:03:24 +0300

    save

--- Java/dataStructure/tree/test.java
+++ Java/dataStructure/tree/test.java
@@ -1,7 +1,5 @@
-
 public class test {
     public static void main(String[] args) {
-        //stack();
         Tree tree=new Tree();
         tree.insert(20);
         tree.insert(10); 
@@ -15,6 +13,7 @@ public class test {
         tree.insert(3); 
         tree.insert(1); 
         tree.insert(2); 
+        
         tree.search(5); 
     }
 }
