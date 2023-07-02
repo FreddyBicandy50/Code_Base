a2ed8988b7ebb94e8c847758e625ee1c3b7137b0
FreddyBicandy50 <fredos112033@gmail.com>
2023-07-02 20:45:11 +0300

    save

--- Python/CS50P/week0.py
+++ Python/CS50P/week0.py
@@ -9,10 +9,24 @@ def playbackspeed():
 
 #Making Faces
 def makingfaces(): 
-    print(convert(input("").split(" ") ))
+	print(convert(input("")))
 
-def convert(message="Hello",icon=":)"):
-    if(iconemote==":)"):iconemote="🙂"
-    elif (iconemote==":("): iconemote="🥺" 
-    return message+" "+iconemote
-makingfaces()
+def convert(message):
+	message,emote=message.split(" ")
+	if(emote==":)"):emote="🙂"
+	elif(emote==":("):emote="🙁"
+	return message+" "+emote
+
+#Einstein
+def einstein():
+    print(formula(int(input("M:")))) 
+def formula(m):
+    return m*pow(300000000,2) 
+  
+ 
+#Tip Calculator
+def TIP():
+     print(calculate(float(input("How much was the meal?:")),int(input("What percentage would you like to tip?:"))))
+def calculate(price,percent): 
+     return f"Leave $ {(price*(1+percent/100))-price:.2f}"
+TIP()
