4449edac8ad745cebe4ad402eedbdc2c4a276b5b
FreddyBicandy50 <fredos112033@gmail.com>
2023-07-28 19:48:09 +0300

    save

--- Python/CS50P/week3.py
+++ Python/CS50P/week3.py
@@ -21,5 +21,50 @@ def FuelGauge(prompt):
         except ZeroDivisionError:
             pass
 
+#Felipe’s Taqueria
+def FelipeT():
+    while True:
+        print(f"Total:{order('item:')}$")
 
 
+def order(prompt):
+    menu={
+        "Baja Taco": 4.00,
+        "Burrito": 7.50,
+        "Bowl": 8.50,
+        "Nachos": 11.00,
+        "Quesadilla": 8.50,
+        "Super Burrito": 8.50,
+        "Super Quesadilla": 9.50,
+        "Taco": 3.00,
+        "Tortilla Salad": 8.00
+    }
+    try:
+        item=input(prompt)
+        if item in menu:
+            return str(menu[item])
+    except EOFError:
+        pass
+
+
+
+
+#Grocery List
+def GroceryList():
+    list=GLIST()
+    for key in list:
+        print(list[key],key)
+def GLIST():
+    list={}
+    while True:
+        try:
+            item=input("").upper()
+            if item.isalpha():
+                if item in list:
+                    list[item]+=1
+                else:
+                    list[item] = 1 
+        except EOFError:
+            return list
+        
+GroceryList()
