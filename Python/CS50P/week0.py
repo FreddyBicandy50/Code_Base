40eefd989de09303056c08a577eb2c9db6e935cc
FreddyBicandy50 <fredos112033@gmail.com>
2023-09-24 19:13:23 +0300

    save

--- Python/CS50P/week0.py
+++ Python/CS50P/week0.py
@@ -6,17 +6,16 @@ def indoorvoice():
 #Playback Speed
 def playbackspeed(): 
     print(input("").replace(" ","..."))
-
 #Making Faces
-def makingfaces(): 
-	print(convert(input("")))
-
 def convert(message):
 	message,emote=message.split(" ")
 	if(emote==":)"):emote="🙂"
 	elif(emote==":("):emote="🙁"
 	return message+" "+emote
 
+def makingfaces(): 
+    print(convert(input("")))
+makingfaces()
 #Einstein
 def einstein():
     print(formula(int(input("M:")))) 
@@ -29,4 +28,3 @@ def TIP():
      print(calculate(float(input("How much was the meal?:")),int(input("What percentage would you like to tip?:"))))
 def calculate(price,percent): 
      return f"Leave $ {(price*(1+percent/100))-price:.2f}"
-TIP()
---
+++ Python/CS50P/week4.py
@@ -0,0 +1,8 @@
+while True:
+    try:
+        print(f"{int(input('enter a number:'))}")
+    except ValueError:
+        print("You did not co-oprate mothar fakar")
+    else:
+        break
+
